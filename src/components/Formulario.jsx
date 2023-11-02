11dbc76046a5e203c5dd7e3c2a544b778b0cca8f
Ramon Martinez <ramon.martinez@tisal.cl>
2023-11-02 10:07:07 -0300

    correciones del estado

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,89 +1,64 @@
 /* eslint-disable no-unused-vars */
-import { useState } from 'react';
+import { useState} from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Alert from './Alert';
 
 const Formulario = () => {
 
-  const FIELD_IS_NAME_REQUIRED = 'El nombre es requerido';
-  const FIELD_IS_EMAIL_REQUIRED = 'El email es requerido';
-  const FIELD_IS_PASS_REQUIRED = 'El password es requerido';
-  
-  const FIELD_IS_EMAIL_INVALID = 'El email no es válido';
-  const PASS_NOT_MATCH = 'Las contraseñas no coinciden';
-
   const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
   const [ nombre, setNombre ] = useState('');
   const [ email, setEmail ] = useState('');
   const [ pass, setPass ] = useState('');
-  const [isSamePass, setIsSamePass] = useState(false);
-
-  const [ errors, setErrors ] = useState({});
-
-
-  const handleName = (e) => { 
-    const { value } = e.target;
-    setNombre(value);
-    if (!value) {
-      setErrors([...errors, FIELD_IS_NAME_REQUIRED ]);
-      return;
-    }
-  };  
-
-  const handleEmail = (e) => { 
-    const { value } = e.target;
-    setEmail(value);
-    if (!value) {
-      setErrors([...errors, FIELD_IS_EMAIL_REQUIRED ]);
-      return;
-    }
-    if (!emailRegex.test(value)) { 
-      setErrors([...errors, FIELD_IS_EMAIL_INVALID]);
-      return;
-    }
-  };  
-
-  const handlePassword = (e) => { 
-    const { value } = e.target;
-    setPass(value);
-    if (!value) {
-      setErrors([...errors, FIELD_IS_PASS_REQUIRED]);
-      return;
-    }
-  }; 
   
-  const handleRepeatPass = (e) => {
-    const { value } = e.target;	
-    if (!value) {
-      setErrors([...errors, PASS_NOT_MATCH]);
-      return
-    }
-    if (value !== pass) {
-      setErrors([...errors, PASS_NOT_MATCH]);
-    }
-    if (errors.includes(PASS_NOT_MATCH)) {
-      const newErrors = errors.filter((error) => error !== PASS_NOT_MATCH);
-      setErrors(newErrors);
-    }
-    setIsSamePass(true);
-  }
+  const [isSamePass, setIsSamePass] = useState(false);
+  const [isValidEmail, setIsValidEmail] = useState(false);
+  
+  const [toucheForm, setToucheForm] = useState(false);
+  const [showAlert, setShowAlert] = useState(false);
 
+  const [errors, setErrors] = useState(false);
+  
   const submit = (e) => {
     e.preventDefault();
-    if (!nombre) { 
-      alert('El nombre es requerido')
-     }
-    if (!email) {
-    alert('El email es requerido')
+    if (!nombre || !email || !pass || !isSamePass) {
+      setShowAlert(true);
+      setErrors(true)
+      return;
     }
-    if (!pass) {
-    alert('El password es requerido')
+    if (!emailRegex.test(email)) {
+      setShowAlert(true);
+      setErrors(true)
+      return;
+    } else {
+      setIsValidEmail(true)
     }
+    if (!isSamePass) {
+      setShowAlert(true);
+      setErrors(true)
+      setIsSamePass(false)
+      return
+    }
+
+    resetState(e);
+    setShowAlert(true);
+    setTimeout(() => {
+      setShowAlert(false);
+    }, 2000);
   }
 
-
+  const resetState = (event) => {
+    event.target.reset();
+    setEmail('');
+    setNombre('');
+    setPass('');
+    setErrors(false);
+    setIsSamePass(false);
+    setIsValidEmail(false);
+    setToucheForm(false);
+   }
+  
   return (
     <div>
       <Form onSubmit={submit} >
@@ -91,16 +66,16 @@ const Formulario = () => {
           type="text"
           name="firstName"
           placeholder="Nombre"
-          value={nombre}
-          onChange={handleName}
+          onChange={e => setNombre(e.target.value)}
+          isInvalid={toucheForm && errors}
         />
         <Form.Group >
           <Form.Control className="mb-3"
             type="email"
             name="email"
             placeholder="tuemail@ejemplo.com"
-            value={email}
-            onChange={handleEmail}
+            onChange={e => setEmail(e.target.value)}
+            isInvalid={toucheForm && (errors)}
           />
         </Form.Group>
         <Form.Group>
@@ -108,24 +83,23 @@ const Formulario = () => {
             type="password"
             name="password"
             placeholder="Contraseña"
-            value={pass}
-            onChange={handlePassword}
+            onChange={e => setPass(e.target.value)}
+            isInvalid={toucheForm && (errors || !isSamePass)}
           />
           <Form.Control className="mb-3"
             type="password"
             name="confirmPassword"
             placeholder="Confirma tu contraseña"  
-            onChange={handleRepeatPass}
+            onChange={e => setIsSamePass(e.target.value === pass)}
+            isInvalid={toucheForm && (errors || !isSamePass)}
           />
         </Form.Group>
           <Button type='submit' variant="success"  className='w-100 mb-2'>
             Registrarse
         </Button>
+      
+         { toucheForm || showAlert ?  <Alert message={errors ? 'debe de completar los datos' : 'Se ha registrado correctamente'  } type={errors ? 'danger' : 'success' } /> : null}
         
-
-        {/* {errors.map(e => {
-          <Alert message={`${e}<br>` } />
-        } ) } */}
         </Form>
     </div>
   );
